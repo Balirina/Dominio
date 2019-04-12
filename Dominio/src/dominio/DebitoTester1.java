df4cf21f18868dd62bc467076e01c277ce3a3a5a
Balirina <47902373+Balirina@users.noreply.github.com>
2019-04-12 13:24:08 +0200

    Clases creadas

---
+++ Dominio/src/dominio/DebitoTester1.java
@@ -0,0 +1,14 @@
+package dominio;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DebitoTester1 {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
